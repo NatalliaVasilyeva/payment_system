3788218c725deb8040b459c0ecdd77d30b166e93
Natallia Vasilyeva <natallia.vasilyeva@sap.com>
2023-12-05 16:54:14 +0300

    skeleton

---
+++ fake-payment-provider/src/test/java/com/proselyte/fakepaymentprovider/FakePaymentProviderApplicationTests.java
@@ -0,0 +1,12 @@
+package com.proselyte.fakepaymentprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakePaymentProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
